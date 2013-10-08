275049e74aa854cc34f71ae7484c955b433d6098
apricote <julian.toelle97@gmail.com>
2013-10-08 21:57:29 +0300

    #14 Fix: Creates new File with Standart Object
    Added updating the objects.txt for #16 and #17

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -3,7 +3,11 @@ package logic;
 import gui.TesseractFrame;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -22,8 +26,6 @@ public class Main {
 		Camera rightCam = new Camera(0, 0, 5, 0, abstractMultipointObjects);
 		TesseractFrame frame = new TesseractFrame(leftCam, rightCam,
 				abstractMultipointObjects);
-
-		// cube.rotate(Math.PI / 50);
 
 		long millis = System.currentTimeMillis();
 
@@ -51,13 +53,23 @@ public class Main {
 
 	public static ArrayList<AbstractMultipointObject> readObjects() {
 
-		String source = "";
+		String source = "Tesseract|16|0.0|0.0|0.0|0.0|4.0|4.0|4.0|4.0|4.0|4.0|4.0|-4.0|4.0|4.0|-4.0|4.0|4.0|4.0|-4.0|-4.0|4.0|-4.0|4.0|4.0|4.0|-4.0|4.0|-4.0|4.0|-4.0|-4.0|4.0|4.0|-4.0|-4.0|-4.0|-4.0|4.0|4.0|4.0|-4.0|4.0|4.0|-4.0|-4.0|4.0|-4.0|4.0|-4.0|4.0|-4.0|-4.0|-4.0|-4.0|4.0|4.0|-4.0|-4.0|4.0|-4.0|-4.0|-4.0|-4.0|4.0|-4.0|-4.0|-4.0|-4.0|0|1|0|2|0|4|1|3|1|5|2|3|2|6|3|7|4|5|4|6|5|7|6|7|8|9|8|10|8|12|9|11|9|13|10|11|10|14|11|15|12|13|12|14|13|15|14|15|0|8|1|9|2|10|3|11|4|12|5|13|6|14|7|15$";
 
 		BufferedReader in;
 		try {
 			in = new BufferedReader(new FileReader("objects.txt"));
 			source = in.readLine();
 			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			try {
+				File file = new File("objects.txt");
+				BufferedWriter output = new BufferedWriter(new FileWriter(file));
+				output.write(source);
+				output.close();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -105,5 +117,41 @@ public class Main {
 
 		}
 		return objects;
+	}
+
+	public static void saveObjects(ArrayList<AbstractMultipointObject> objects) {
+
+		String saveString = "";
+
+		for (AbstractMultipointObject object : objects) {
+			saveString += object.NAME + "|";
+			saveString += object.VERTICES + "|";
+			saveString += object.getX() + "|" + object.getY() + "|"
+					+ object.getZ() + "|" + object.getW();
+
+			for (Dot dot : object.getDots()) {
+				saveString += "|" + dot.getX() + "|" + dot.getY() + "|"
+						+ dot.getZ() + "|" + dot.getW();
+			}
+
+			int[][] connectedVertices = object.getConnectedVertices();
+
+			for (int i = 0; i < connectedVertices.length; i++) {
+				saveString += "|" + connectedVertices[i][0] + "|"
+						+ connectedVertices[i][1];
+			}
+			saveString += "$";
+
+		}
+
+		try {
+			File file = new File("objects.txt");
+			BufferedWriter output = new BufferedWriter(new FileWriter(file));
+			output.write(saveString);
+			output.close();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
 	}
 }
